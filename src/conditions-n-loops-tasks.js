9c62c978a5e30ce54ff9b0cb4e51fe97495a3e8b
Dmitry Andriyanov <facklak@mail.ru>
2024-01-12 15:12:34 +0300

    feat: did isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -254,8 +254,17 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let str = '';
+  str += num;
+  let strDigit = '';
+  strDigit += digit;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === strDigit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
